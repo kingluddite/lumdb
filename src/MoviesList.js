cfbb418074768b36665055e25506220220306686
Kingluddite <howley.phil@gmail.com>
2020-02-29 14:58:08 -0800

    initialize repo

---
+++ src/MoviesList.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+
+import Movie from './Movie';
+
+class MoviesList extends Component {
+  state = {
+    movies: [],
+  };
+
+  async componentDidMount() {
+    try {
+      const res = await fetch(
+        'https://api.themoviedb.org/3/discover/movie?api_key=6bed0f87158ec0aba138db3ce714fbdd&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1',
+      );
+      const movies = await res.json();
+      console.log(movies);
+      this.setState({
+        movies: movies.results,
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  }
+  render() {
+    return (
+      <MovieGrid>
+        {this.state.movies.map(movie => <Movie key={movie.id} movie={movie} />)}
+      </MovieGrid>
+    );
+  }
+}
+
+export default MoviesList;
+
+const MovieGrid = styled.div`
+  display: grid;
+  padding: 1rem;
+  grid-template-columns: repeat(6, 1fr);
+  grid-row-gap: 1rem;
+`;
